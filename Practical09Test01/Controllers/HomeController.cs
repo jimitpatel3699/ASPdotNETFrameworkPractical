bc6aeeedf0d21e4c4f209a69257bf35a966c0430
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-06-13 10:26:33 +0530

    Add project files.

---
+++ Practical09Test01/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical09Test01.Controllers
+{
+    [RoutePrefix("")]
+    public class HomeController : Controller
+    {
+        [Route()]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("About")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "application description page.";
+            return View();
+        }
+        [Route("Show")]
+        public string Show()
+        {
+            return "Show method of Home Controller.";
+        }
+        [Route("Data/{id:int}")]
+        public string Data(int id)
+        {
+            return $"Data method of Home Controller.\nid:{id}";
+            
+        }
+    }
+}
