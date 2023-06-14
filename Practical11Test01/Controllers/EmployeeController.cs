90303c3d31dd5b231cb5db56e0821df89e2b2bdb
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-06-14 19:22:21 +0530

    Practical11Test01 perform ceate and view

---
+++ Practical11Test01/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using Practical11Test01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practical11Test01.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private static List<Employee> Employees = new List<Employee>();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(Employees);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee employee)
+        {
+            if(employee.Dob.Date==DateTime.Now.Date) 
+            {
+                ModelState.AddModelError("Invaliddate", "Invalid Dateof Birth");
+            }
+            if(ModelState.IsValid) 
+            {
+                Guid empid = Guid.NewGuid();
+                TempData["status"] = "Employee added.";
+                employee.Id = empid;
+                Employees.Add(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Practical11Test01/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Practical11Test01.Models
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Date of Birth is required")]
+        [DataType(DataType.Date)]
+        public DateTime Dob { get; set; }
+        [Required(ErrorMessage = "Mobile number required")]
+        [StringLength(10,MinimumLength =10)]
+        public string Mobile { get; set; }
+        [Required(ErrorMessage = "Address required")]
+        public string Address { get; set; }
+        
+    }
+}
