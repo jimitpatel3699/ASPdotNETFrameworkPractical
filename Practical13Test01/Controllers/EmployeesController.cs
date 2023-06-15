7a7d4d905c0d59ce797e18507f7ff78a3db068c3
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-06-15 19:10:07 +0530

    Practical13Test01

---
+++ Practical13Test01/Controllers/EmployeesController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Practical13Test01.Data;
+using Practical13Test01.Models;
+
+namespace Practical13Test01.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private ApplicationContext _context = new ApplicationContext();
+        public ActionResult Index()
+        {
+            return View(_context.Employees.ToList());
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employee employee)
+        {
+            if(employee.DateofBirth.Date==DateTime.Now.Date)
+            {
+                ModelState.AddModelError("invalidDob", "Invalid Date of Birth.");
+            }
+            else if(DateTime.Now.Year - employee.DateofBirth.Year < 18 )
+            {
+                ModelState.AddModelError("invalidage", "Employee age must be greater than 18.");
+            }
+            if (ModelState.IsValid)
+            {
+                employee.Age=DateTime.Now.Year- employee.DateofBirth.Year;
+                _context.Employees.Add(employee);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee employee)
+        {
+            if (employee.DateofBirth.Date == DateTime.Now.Date)
+            {
+                ModelState.AddModelError("invalidDob", "Invalid Date of Birth.");
+            }
+            else if (DateTime.Now.Year - employee.DateofBirth.Year < 18)
+            {
+                ModelState.AddModelError("invalidage", "Employee age must be greater than 18.");
+            }
+            if (ModelState.IsValid)
+            {
+                employee.Age = DateTime.Now.Year - employee.DateofBirth.Year;
+                _context.Entry(employee).State = EntityState.Modified;
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = _context.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = _context.Employees.Find(id);
+            _context.Employees.Remove(employee);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Practical13Test01/Migrations/202306151112462_TableCreated.cs
@@ -0,0 +1,28 @@
+﻿namespace Practical13Test01.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TableCreated : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false, maxLength: 50),
+                        DateofBirth = c.DateTime(nullable: false),
+                        Age = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
