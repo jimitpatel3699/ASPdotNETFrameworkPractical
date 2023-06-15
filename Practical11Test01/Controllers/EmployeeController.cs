1869024d84c404c00fa6ab8607de110a50e19c14
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-06-15 12:41:03 +0530

    added update, delete functionality.

--- Practical11Test01/Controllers/EmployeeController.cs
+++ Practical11Test01/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using Practical11Test01.Models;
+﻿using Microsoft.Ajax.Utilities;
+using Practical11Test01.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,9 +23,13 @@ namespace Practical11Test01.Controllers
         [HttpPost]
         public ActionResult Create(Employee employee)
         {
-            if(employee.Dob.Date==DateTime.Now.Date) 
+            if(employee.Dob.Date== DateTime.Now.Date)
             {
-                ModelState.AddModelError("Invaliddate", "Invalid Dateof Birth");
+                ModelState.AddModelError("Invaliddate", "Invalid date.");
+            }
+           else if(DateTime.Now.Year-employee.Dob.Year<18) 
+            {
+                ModelState.AddModelError("Invalidage", "Employee age must be greater than 18.");
             }
             if(ModelState.IsValid) 
             {
@@ -35,6 +40,41 @@ namespace Practical11Test01.Controllers
                 return RedirectToAction("Index");
             }
             return View();
+        }
+        public ActionResult  Edit(Guid id)
+        {
+            Employee emp = Employees.Find(x=>x.Id==id);
+            return View(emp);
+        }
+        [HttpPost]
+        public ActionResult Edit(Employee employee)
+        {
+            if (employee.Dob.Date == DateTime.Now.Date)
+            {
+                ModelState.AddModelError("Invaliddate", "Invalid date.");
+            }
+            else if (DateTime.Now.Year - employee.Dob.Year < 18)
+            {
+                ModelState.AddModelError("Invalidage", "Employee age must be greater than 18.");
+            }
+            if (ModelState.IsValid)
+            {
+                TempData["status"] = "Employee updated.";
+                Employee emp = Employees.Find(x => x.Id == employee.Id);
+                emp.Name = employee.Name;
+                emp.Dob = employee.Dob;
+                emp.Mobile = employee.Mobile;
+                emp.Address = employee.Address;
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public ActionResult Delete(Guid id) 
+        {
+            Employee emp = Employees.Find(x => x.Id == id);
+            Employees.Remove(emp);
+            TempData["status"] = "Employee Deleted.";
+            return RedirectToAction("Index");
         }
     }
 }
